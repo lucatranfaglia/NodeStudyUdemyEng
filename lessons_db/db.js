98c8619f3013a42ffe85fe48af2a488b1e538b33
Luca Tranfaglia <luca.tranfaglia@iquii.com>
2019-04-13 19:02:48 +0200

    MongoDB - DeleteOne , DeleteMany [Promises]

--- lessons_db/db.js
+++ lessons_db/db.js
@@ -15,81 +15,31 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) =>{
 
     const db = client.db(databaseName);
     
-    // ---------------------
-    // METODO con callback 
-    // ---------------------
-
-    //updateOne(filter, update, options)
-    // updateOne - modificato il primo risultato che soddisfa la condizione (ObjectId) 
-    // db.collection('task').updateOne({
-    //     _id : new ObjectID("5cb1d64f665d2c548eb9fab0")
-    // },{
-    //     $set: {
-    //         completed : false
-    //     }
-    // }, (error, result)=>{
-    //     if(error){
-    //         console.log("Error: ",error);
-    //     }
-    //     console.log("Success: ", result);
-    // })
-
-
-    // ---------------------
-    // METODO con Promises 
-    // ---------------------
-    // Primo step
-    // const updatePromises = db.collection('task').updateOne({
-    //     _id : new ObjectID("5cb1d64f665d2c548eb9fab0")
-    // },{
-    //      // UPDATE operators   
-    //
-    //      // setta il valore inserito nel campo selezionato
-    //     $set: {
-    //         completed : true
-    //     }
-    //  // incrementa il valore
-    //  // $inc: {
-    //  //     age: 1  //age: -1
-    //  // }
-    // });
-    
-    // updatePromises
-    // .then( (result)=>{
-    //     console.log("Update: ", result );
-    // })
-    // .catch((error)=>{
-    //     console.log("Error: ", error);
-    // })
-
-    // Secondo step
-    const updatePromises = db.collection('task').updateOne({
-        _id : new ObjectID("5cb1d64f665d2c548eb9fab0")
-    },{
-        $set: {
-            completed : false
+    // DELETE MANY - PROMISE
+    db.collection('users').deleteMany(
+        {
+            age: 22
         }
-    })
-    .then( (result)=>{
-        console.log("Update: ", result );
+    )
+    .then((result)=>{
+        console.log("DeleteMany:", result.deletedCount);
     })
     .catch((error)=>{
         console.log("Error: ", error);
     })
 
 
-    const updateManyPromises = db.collection('task').updateMany({
-        completed : false
-    },{
-        $set: {
-            completed : true
+    // DELETE ONE
+    db.collection('task').deleteOne(
+        {
+            description: 'Mongo DB'
         }
-    })
-    .then( (result)=>{
-        console.log("Update: ", result.modifiedCount );
+    )
+    .then((result)=>{
+        console.log("DeleteOne:", result.deletedCount);
     })
     .catch((error)=>{
         console.log("Error: ", error);
     })
-    
+
 })
