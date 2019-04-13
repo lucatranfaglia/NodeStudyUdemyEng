74ccbd4ad4216a8b16f5109f280725185582eedb
Luca Tranfaglia <luca.tranfaglia@iquii.com>
2019-04-13 14:56:32 +0200

    MongoDB - ObjectID

--- lessons_db/db.js
+++ lessons_db/db.js
@@ -1,40 +1,16 @@
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+
+const { MongoClient , ObjectID } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:32769';
 const databaseName = 'task-manager';
 
-MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) =>{
-    if(error){
-        return console.log('Unable to connect to database!');
-    }
-    console.log('Connect correct');
+const id = new ObjectID();
+// Mongo genera un ID univoco con all'itreno alcune informazioni (tra cui un TIMESTAMP) [es. 5cb1d944edddce552cf0b430]
+console.log(id);
+// ottengo il timestamp di quando è stato creato l'ID
+console.log(id.getTimestamp());
 
-    const db = client.db(databaseName);
-    
-    // all'interno del database 'task-manager' creo una nuova collezione (task) e ci inserisco molti documenti
-    db.collection('task').insertMany([
-        {
-            description: 'Parse CSV',
-            completed: true
-        },
-        {
-            description: 'Arrow function',
-            completed: false
-        },
-        {
-            description: 'Mongo DB',
-            completed: true
-        },
-        {
-            description: 'Mongoose',
-            completed: false
-        },
-    ], (error, result)=>{
-        if(error){
-            return console.log('Unable to insert tasks!')
-        }
-        console.log("Connect");
-        console.log(result.ops);
-    })
-})
+// numero di byte dell'ID [12byte]
+console.log(id.id.length);
+// numbero di byte se converto l'ID in STRING [24]
+console.log(id.toHexString().length);
