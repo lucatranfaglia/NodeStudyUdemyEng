c8b232e501b9e5026aeedf62e852b05f946bf66f
Luca Tranfaglia <luca.tranfaglia@iquii.com>
2019-04-14 12:18:48 +0200

    Mongoose - model with validator, validate

--- mongoose_example/src/db/mongoose.js
+++ mongoose_example/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 pathDb = 'mongodb://127.0.0.1:32769/';
 nameDb = 'task-manager-api';
@@ -13,25 +14,55 @@ mongoose.connect(pathDb+nameDb , {
 // secondo parametro : definizione dei campo
 const User = mongoose.model('User',{
     name:{
-        type: String
+        type: String,
+        require: true,
+        trim : true
+    },
+    email:{
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validator(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid');
+            }
+        }
+    },
+    password:{
+        tyoe:String,
+        required: true,
+        minlenght: 7,
+        trim: true,
+        validator(value){
+            if(value.toLowercase().includes("password")){
+                throw new Error('Password cannot contain "password"');
+            }
+        }
     },
     age:{
-        type: Number
+        type: Number,
+        default: 0,
+        // funzione che mi permette di personalizzare (tramite condizione) il valore da ottenere (es. non voglio numeri negativi)
+        validate(value){
+            if(value<0){
+                throw new Error('Age must be a positive number');
+            }
+        }
     }
 
 })
 
-// creo un istanza con modello User
 const utente = new User({
-    name: 'Bilbo',
-    age: 110
-});
+    name: 'Gandalf   ',
+    email: 'gandalf.ilbianco@wizard.com',
+    age: 100                            // se inserisco il eta: '100' => di default inseri age: 0
+})
 
-// per salvare l'istanza sul DB uso la funzione save()
 utente.save()
 .then((result)=>{
-    console.log("Save: ", result);
+    console.log("Save: ",result);
 })
 .catch((error)=>{
-    console.log("Error: ",error);
+    console.log("Error: ", error);
 })
